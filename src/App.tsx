cf3ec4f067eb5b6d5d2ae9da4b2c04e8c59645a9
vino <vino0908@outlook.com>
2022-11-04 17:02:32 +0800

    style: 主题切换方案

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState("dark")
+  const handleClick = (theme: 'dark' | 'light' | '') => {
+    setTheme(theme)
+    const root = window.document.documentElement
+    if (theme === 'dark') {
+      root.classList.remove('light')
+      root.classList.add('dark')
+    } else if (theme === 'light') {
+      root.classList.remove('dark')
+      root.classList.add('light')
+    } else {
+      root.classList.remove('dark')
+      root.classList.remove('light')
+    }
+    localStorage.setItem('theme', theme)
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {JSON.stringify(theme)}
+      <button onClick={() => handleClick('dark')}>dark</button>
+      <button onClick={() => handleClick('light')}>light</button>
+      <button onClick={() => handleClick('')}>跟随系统</button>
     </div>
   )
 }
