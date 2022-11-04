87521b2123823d08e07fc1889314e613707ad238
vino <vino0908@outlook.com>
2022-11-04 17:08:18 +0800

    chore: 文件位置调整

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import './assets/theme.css'
+import "./assets/style/index.css"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
