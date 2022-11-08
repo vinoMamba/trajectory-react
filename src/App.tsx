61d8580cb312e4554aff4cd3e6f05cff5eda4c36
vino <vino0908@outlook.com>
2022-11-08 17:41:18 +0800

    chore: 添加 react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { useDark } from './hooks/useDark'
+import { Outlet } from "react-router-dom"
 
 export const App = () => {
   const [_, toggleDark] = useDark()
   return (
     <div className="App">
       <button onClick={() => toggleDark()}>toggle</button>
+      <Outlet />
     </div >
   )
 }
