745be2e7cfe15e5fd7eda26c8684e10bba9f37f7
vino <vino0908@outlook.com>
2022-11-08 18:03:05 +0800

    chore: 集成 material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import { useDark } from './hooks/useDark'
-import { Outlet } from "react-router-dom"
+import { TextField } from '@mui/material';
 
 export const App = () => {
   const [_, toggleDark] = useDark()
   return (
     <div className="App">
       <button onClick={() => toggleDark()}>toggle</button>
-      <Outlet />
+      <TextField label="邮箱" variant="standard" placeholder="请输入邮箱" />
+      <TextField label="验证码" variant="standard" placeholder="验证码" />
     </div >
   )
 }
